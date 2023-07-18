0bf6d2d3243e278a754c3753e420aa6ebf8649be
Gunta Skrastina <gunta.skrastina@gmail.com>
2023-07-18 17:55:41 +0200

    use explicit cast

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gskrasti <gskrasti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 19:38:43 by gskrasti          #+#    #+#             */
-/*   Updated: 2023/07/17 19:46:01 by gskrasti         ###   ########.fr       */
+/*   Updated: 2023/07/18 16:29:12 by gskrasti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int main(void)
     int b = 3;
     ::swap(a, b);
     std::cout << "a = " << a << ", b = " << b << std::endl;
-    std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
-    std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+    std::cout << "min(a, b) = " << ::min<int>(a, b) << std::endl;
+    std::cout << "max(a, b) = " << ::max<int>(a, b) << std::endl;
 
     std::string c = "chaine1";
     std::string d = "chaine2";
     ::swap(c, d);
     std::cout << "c = " << c << ", d = " << d << std::endl;
-    std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
-    std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+    std::cout << "min(c, d) = " << ::min<std::string>(c, d) << std::endl;
+    std::cout << "max(c, d) = " << ::max<std::string>(c, d) << std::endl;
 
     return (0);
 }
