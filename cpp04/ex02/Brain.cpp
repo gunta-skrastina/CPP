1436257881dd75326f3bf5cc77a34ed859b10ae2
gunta-skrastina <57624291+gunta-skrastina@users.noreply.github.com>
2023-07-25 20:22:40 +0200

    Update Brain.cpp

--- cpp04/ex02/Brain.cpp
+++ cpp04/ex02/Brain.cpp
@@ -17,21 +17,24 @@ Brain::Brain()
     std::cout << "Brain constructor called" << std::endl;
 }
 
-Brain::Brain(const Brain& brain)
+Brain::Brain(const Brain& other)
 {
     std::cout << "Brain copy constructor called" << std::endl;
     for (int i = 0; i < 100; i++)
     {
-        this->ideas[i] = brain.ideas[i];
+        ideas[i] = other.ideas[i];
     }
 }
 
-Brain& Brain::operator=(const Brain& brain)
+Brain& Brain::operator=(const Brain& other)
 {
     std::cout << "Brain copy assignement constructor called" << std::endl;
-    for (int i = 0; i < 100; i++)
+    if (this != & other)
     {
-        this->ideas[i] = brain.ideas[i];
+        for (int i = 0; i < 100; i++)
+        {
+            ideas[i] = other.ideas[i];
+        }
     }
     return (*this);
 }
