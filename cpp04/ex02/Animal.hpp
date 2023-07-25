10ba1715518273de2a10badf270064db8c9166a4
gunta-skrastina <57624291+gunta-skrastina@users.noreply.github.com>
2023-07-25 20:20:57 +0200

    Update Animal.hpp

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -18,15 +18,15 @@
 class Animal
 {
 protected:
-    std::string type;
+	std::string type;
 public:
-    Animal();
-    Animal(const Animal& animal);
-	Animal& operator=(const Animal& animal);
-    virtual ~Animal();
-    
-    virtual void makeSound() const = 0;
-    std::string getType() const;
+	Animal();
+	Animal(const Animal& other);
+	Animal& operator=(const Animal& other);
+	virtual ~Animal();
+	
+	virtual void makeSound() const = 0;
+	std::string getType() const;
 };
 
 #endif
